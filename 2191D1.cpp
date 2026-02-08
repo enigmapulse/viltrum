cfd2ad7ddca72e1b166e94c215364ce79e21b2d7
enigmapulse <navneetkashyap2006@gmail.com>
2026-02-08 11:16:53 +0530

    abc444 and practice

---
+++ 2191D1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define inarr(a, n) for (int _i = 0; _i < (n); _i++) cin >> (a)[_i];
+#define invec(v) for (auto &_x : (v)) cin >> _x;
+
+using namespace std;
+
+bool multipleTests = true;
+
+void solve() {
+    ll n; cin >> n;
+    string s; cin >> s;
+    for (ll i = 0; i < n - 4; i++) {
+        if(s.substr(i, 3) == ")((") {
+            cout << n - 2 << endl;
+            return;
+        }
+    }
+    cout << -1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    if (multipleTests)
+        cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
