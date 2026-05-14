d32130b4a3b9762a8f88f41d5dfeff5c67a47a31
enigmapulse <navneetkashyap2006@gmail.com>
2026-05-14 21:11:52 +0530

    1089_div2 (pending c2)

---
+++ Codeforces/Contests/Round_1089_Div2/2210C1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define inarr(a, n) for (int _i = 0; _i < (n); _i++) cin >> (a)[_i];
+#define invec(v) for (auto &_x : (v)) cin >> _x;
+
+using namespace std;
+
+bool multipleTests = true;
+
+void solve() {
+    ll n; cin >> n;
+    vector<ll> a(n); inarr(a, n); inarr(a, n);
+
+    ll cnt = 0;
+    for (ll i = 1; i < n - 1; i++) {
+        ll g1 = gcd(a[i], a[i - 1]);
+        ll g2 = gcd(a[i], a[i + 1]);
+        ll m = lcm(g1, g2);
+        if(m < a[i]) cnt++;
+    }
+    
+    if(gcd(a[0], a[1]) != a[0]) cnt++;
+    if(gcd(a[n - 1], a[n - 2]) != a[n - 1]) cnt++;
+
+    cout << cnt << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    if (multipleTests)
+        cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
